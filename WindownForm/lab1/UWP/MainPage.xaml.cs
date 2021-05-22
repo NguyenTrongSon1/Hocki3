3641c3a75dc3d473023e7a7ce246aaa3f33f203e
NguyenTrongSon1 <sonntth2003006@fpt.edu.vn>
2021-05-22 09:17:55 +0700

    lab 1

---
+++ WindownForm/lab1/UWP/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        public void Button_Click(object sender,RoutedEventArgs e)
+        {
+          
+            string email = Email.Text;
+            string title = Title.Text;
+            string content = Content.Text;
+            TxtBlock.Text += "To:" + email + "\n" + "Title:" + title + "\n" + "Content:" + content + "\n";
+        }
+    }
+}
